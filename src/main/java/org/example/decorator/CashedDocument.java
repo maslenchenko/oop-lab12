5b16731418ef269a6e3287b1bac43645203a49ca
oleksandra <91615687+maslenchenko@users.noreply.github.com>
2022-12-22 15:15:27 +0200

    Update CashedDocument.java

--- src/main/java/org/example/decorator/CashedDocument.java
+++ src/main/java/org/example/decorator/CashedDocument.java
@@ -1,4 +1,17 @@
 package org.example.decorator;
 
-public class CashedDocument {
+public class CashedDocument implements Document {
+
+    private final Document document;
+    private String cashe;
+
+    public CashedDocument(Document document) {
+        this.document = document;
+    }
+    
+    @Override
+    public String parse() {
+        this.cashe = document.parse();
+        return cashe;
+    }
 }
